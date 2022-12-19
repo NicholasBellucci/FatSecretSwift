70bca40901bb1d3b11e4e042f4dfcad9f47610fe
Fernando Botelho <fmb4800@gmail.com>
2022-12-19 16:19:47 -0500

    Add an FBError enum type to FatSecretError.swift, that will be used in recipe search requests when dealing with the Result type.

--- Sources/FatSecretSwift/Models/FatSecretError.swift
+++ Sources/FatSecretSwift/Models/FatSecretError.swift
@@ -2,3 +2,10 @@ public struct FSError: Codable {
     let code: Int
     let message: String?
 }
+
+
+public enum FBError: String, Error {
+    case unableToComplete       = "Unable to comple request. Please check internet connection."
+    case invalidResponse        = "Invalid response from server. Try again."
+    case invalidData            = "The data received from server was invalid. Try again."
+}
